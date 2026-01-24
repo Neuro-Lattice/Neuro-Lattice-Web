9248dc978850ad26ad9aadf57cee5ee3b91fec24
satyam <satyam>
2026-01-25 03:00:21 +0530

    Add OG/Twitter image meta tags for SEO

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -12,10 +12,13 @@ const SEO = ({ title, description }: SEOProps) => {
       <meta property="og:type" content="website" />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
+      <meta property="og:image" content="https://neuro-lattice.com/favicon.png" />
+      
       {/* Twitter */}
       <meta property="twitter:card" content="summary_large_image" />
       <meta property="twitter:title" content={title} />
       <meta property="twitter:description" content={description} />
+      <meta property="twitter:image" content="https://neuro-lattice.com/favicon.png" />
     </>
   );
 };
